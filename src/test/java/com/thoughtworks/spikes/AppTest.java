b1ee113b370e64274f81e7c95f5f4593222312b4
Avinash Chugh <avinash.chugh@gmail.com>
2013-06-28 09:31:27 +0530

    initial scaffolding

---
+++ src/test/java/com/thoughtworks/spikes/AppTest.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.spikes;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+  public AppTest(String testName) {
+    super(testName);
+  }
+
+  public void testApp() {
+    assertTrue(true);
+  }
+}
